f7c959b38be83ffe744cfc5d6d21ef39215a400c
EliasCoumine <e.coumine@gmail.com>
2025-01-22 13:39:52 -0500

    Changes to HTTP responses

---
+++ CS50W/DJ/l3/hello/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .  import views
+
+urlpatterns = [
+    path("", views.index, name ="index"),
+    path("<str:name>", views.greet, name = "greet"),
+    path("elias", views.elias, name = "elias"),
+    path("ronaldo", views.ronaldo, name = "ronaldo")
+
+
+
+]
--- CS50W/DJ/l3/hello/views.py
+++ CS50W/DJ/l3/hello/views.py
@@ -4,7 +4,16 @@ from django.shortcuts import render
 # Create your views here. something thet user might want to see
 
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, "hello/index.html")
+
+def elias(request):
+    return HttpResponse("Hello Coumine")
+
+def ronaldo(request):
+    return HttpResponse("sui")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()} !")
 
 
 
